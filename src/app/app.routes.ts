33e355a7b2a9372e6c341835396d3139668f373e
Tatetinho <141674380+Tatetinho@users.noreply.github.com>
2025-09-07 03:07:39 -0300

    update meus liks component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeNg } from './home-ng/home-ng';
 import { HeaderNg } from './header-ng/header-ng';
 import { FooterNg } from './footer-ng/footer-ng';
 import { DashboardNg } from './dashboard-ng/dashboard-ng';
+import { MeusLinks } from './meus-links/meus-links';
 
 export const routes: Routes = [
      {path:'',component:HomeNg},
      {path:'dashboard',component:DashboardNg},
+     {path:'meusLinks',component:MeusLinks}
 
 
 ];
